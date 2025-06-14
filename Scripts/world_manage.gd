d15c11e20c07d15a037dd3278611ecfbe94301cd
Salsa177 <salvatoregtumminia@gmail.com>
2025-06-14 13:04:43 +1000

    sd

--- Scripts/world_manage.gd
+++ Scripts/world_manage.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var prompt: PackedStringArray = TDF.parent_array.get(TDF.RNG_prompt())
-var current_word: String
-var current_prompt := prompt
-@onready var prompt_label: RichTextLabel = $Prompt_Label
+
+var current_prompt: PackedStringArray
+@onready var prompt_label: RichTextLabel = %Prompt_Label
+@onready var typing_label: RichTextLabel = %Typing_Label
 var current_index: int = 0
 var player_input_string: String
 var key_typed: String
@@ -14,38 +14,37 @@ var wpm_PB: float
 var backspace_buffer: int = 0
 var test: String = "hello my name is the goat romeo and my love is juliet"
 var timer_index: int = 0
-var max_time: float = 10.0
+var max_time: float = 1000.0
 var total_timer: float = 0
 var can_control: bool = true
+var changed_prompt: PackedStringArray
 
 
 func _ready() -> void:
 	current_index = 0
+	get_prompt()
 	print(current_prompt)
 	print(current_index)
-	print(prompt)
-	
-	current_word = prompt.get(current_index)
-	prompt_label.text = "[center]" + current_word + "[/center]"
-#" ".join(current_prompt)
+	print(current_prompt)
 
 
 func _process(delta: float) -> void:
 	#print(test.split(" ", false))
-	update_label()
 	timing_typed_words(delta)
+	update_label()
 	#next_prev_word()
 
 
 func get_prompt() -> void:
-	pass
+	var prompt: PackedStringArray = TDF.parent_array.get(TDF.RNG_prompt())
+	current_prompt = prompt
 
 
 func update_label() -> void:
 	if can_control == false:
-		prompt_label.text = "words per minute: " + str(wpm_average)
+		typing_label.text = "words per minute: " + str(wpm_average)
 	else:
-		prompt_label.text = player_input_string
+		set_type_label(current_prompt)
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
@@ -79,8 +78,55 @@ func timing_typed_words(delta: float) ->void:
 		current_index = 0
 	elif Input.is_action_pressed("Spacebar"):
 		current_index = PIA.size()
-	elif Input.is_action_pressed("Backspace"):
+	elif PIA.size() < current_index:
 		current_index = PIA.size()
+	elif PIA.size() == current_index:
+		if Input.is_action_pressed("Backspace") and (
+			PIA.is_empty() == false) and (
+			current_index != PIA.size() and (
+			PIA.get(current_index).is_empty() == false)) and (
+			len(PIA.get(current_index)) < len(changed_prompt.get(current_index))) and (
+			len(PIA.get(current_index)) > 1):
+				current_index = PIA.size()
+
+
+func set_type_label(prompt: PackedStringArray) -> void:
+	var PIA: PackedStringArray = player_input_string.split(" ", 0)
+	var current_word := prompt.get(current_index)
+	var char_index: int = 0
+	#var current_char: String = changed_prompt.get(current_index).substr(char_index, 1)
+	var word_length: int
+	
+
+	changed_prompt = prompt
+	
+	if (PIA.is_empty() == false) and (current_index != PIA.size()):
+		changed_prompt = prompt
+		
+		
+		if PIA.get(current_index).is_empty() == false:
+			word_length = len(PIA.get(current_index))
+			print(PIA.get(current_index).substr(word_length))
+		
+		if len(PIA.get(current_index)) > len(prompt.get(current_index)):
+			changed_prompt.set(current_index, PIA.get(current_index))
+		else:
+			if PIA.get(current_index).is_empty() == false:
+				changed_prompt.set(current_index, 
+					str(PIA.get(current_index) + changed_prompt.get(current_index).erase(0, word_length)))
+		
+		#for char: String in changed_prompt.get(current_index):
+			#if current_char == 
+	
+	
+	typing_label.text = " ".join(changed_prompt)
+	
+	print(current_index)
+	print(player_input_string)
+	print(changed_prompt)
+	print(prompt)
+	print(current_prompt)
+
 
 	if timer_index != current_index:
 		if current_index == (wpm_stored_times.size() + 1):
@@ -94,6 +140,10 @@ func timing_typed_words(delta: float) ->void:
 			timer_index = current_index
 			wpm_stored_times.append(wpm_word_timer)
 			wpm_word_timer = 0
+	
+	if current_index >= current_prompt.size():
+		current_index = current_prompt.size()
+		can_control = false
 	
 	if total_timer >= max_time:
 		can_control = false
